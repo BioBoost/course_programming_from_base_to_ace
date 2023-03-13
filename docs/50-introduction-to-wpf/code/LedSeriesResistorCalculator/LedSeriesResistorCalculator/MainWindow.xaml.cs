93efd85870f64d4695e156afc021743f035cd505
Nico De Witte <nico.dewitte@outlook.com>
2023-03-13 13:47:41 +0100

    Add LED series resistor calculator app which can be used as a class demo

---
+++ docs/50-introduction-to-wpf/code/LedSeriesResistorCalculator/LedSeriesResistorCalculator/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LedSeriesResistorCalculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void solve_Click(object sender, RoutedEventArgs e)
+        {
+            double vcc = Convert.ToDouble(supplyVoltage.Text);
+            double forward = Convert.ToDouble(ledVoltage.Text);
+            double iCurrent = Convert.ToDouble(current.Text);
+
+            double rForward = 1000 * (vcc - forward) / iCurrent;
+
+            resistorVoltage.Text = (vcc - forward).ToString();
+            resistance.Text = rForward.ToString();
+        }
+    }
+}
---
+++ docs/50-introduction-to-wpf/code/LedSeriesResistorCalculator/README.md
@@ -0,0 +1,3 @@
+# LED Series Resistor Calculator
+
+Simple demo for the class. This was created as a preparation for the course lesson on WPF basics.
